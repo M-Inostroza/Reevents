9546888e722ddae43d0761c90fcc1539c5d1932b
Mauricio Inostroza <mautrode@gmail.com>
2022-01-12 18:10:24 +0100

    End of section 1

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App/layout/style.css';
+import App from './App/layout/App';
+import reportWebVitals from './reportWebVitals';
+
+const rootEl = document.getElementById('root');
+
+function render() {
+  ReactDOM.render(
+    <App />, rootEl
+  );
+}
+
+if (module.hot) {
+  module.hot.accept('./App/layout/App.jsx', function() {
+    setTimeout(render);
+  })
+}
+
+render();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
